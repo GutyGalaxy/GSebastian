18b70e924b16a26720058bbd3acfb19274d62595
BRojas <benja.rojas06@hotmail.com>
2018-05-03 18:54:20 -0300

    lab3

---
+++ lab3ej2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void pago(int meses, int aumento_de_la_cuota);
+    int i, contador=0, valor;
+    for (i=0, i>19, i++)
+{   valor=10
+	contador= valor*2;
+}
+printf ("%d", cuota);
+int main ()
+{ 
+	
+    
